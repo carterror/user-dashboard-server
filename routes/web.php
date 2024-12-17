2b2b396bce82568c61c16ca47e3bda008a64342c
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2024-12-16 21:08:05 -0500

    fix redirect primary

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,19 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/', function () {
-    return redirect('/api/openapi');
+    return redirect('/openapi');
 });
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/stats', [UserController::class, 'stats']);
